d10aa39d50dfa06026ae198f83e9bd55c3631e8a
dillionzhang <dillionzhang@xiaoe-tech.com>
2022-01-05 19:36:35 +0800

    fix: 删除无用代码

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,12 +1,10 @@
-
 <template>
     <div>
         <h1>这是关于页面</h1>
-        <CustomAni v-model="score"/>
-        {{score}}
+        <CustomAni v-model="score" />
+        {{ score }}
         <!-- <CustomAni :value="1" theme='red'/>
         <CustomAni :value="5" theme='green'/> -->
-
 
         <!-- 动画 -->
         <div>
@@ -15,17 +13,16 @@
             </transition>
             <button @click="showTitle = !showTitle">点击</button>
         </div>
-        
     </div>
 </template>
 
 <script setup>
 import CustomAni from '../components/CustomAni.vue';
-import {ref, defineProps} from 'vue'
-let score = ref(3)
-let showTitle = ref(false)
+import { ref, defineProps } from 'vue';
+let score = ref(3);
+let showTitle = ref(false);
 function update(num) {
-    score.value = num
+    score.value = num;
 }
 // let props = defineProps({
 //     modelValue: Number,
@@ -35,7 +32,13 @@ function update(num) {
 // })
 </script>
 
-
 <style scoped>
-    .fade-enter-active,.fade-leave-active { transition: opacity 0.5s linear;}.fade-enter-from,.fade-leave-to { opacity: 0;}
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s linear;
+}
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
 </style>
